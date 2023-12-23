909e74600c5581752d390443b0f923beadb69d01
Jason Jack <jasonajack@gmail.com>
2023-12-22 22:00:06 -0500

    Added yamllint

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ rm -rf ~/.cache/enphase-envoy/
 poetry run black --check envoy_logger tests
 poetry run isort --check envoy_logger tests
 poetry run flake8 envoy_logger tests
+poetry run yamllint -c .yamllint .
 poetry run coverage run -m pytest -s
 poetry run coverage report -m
 
